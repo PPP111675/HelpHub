3bd7e26a2907c09ce0fabbe3203d9fe9d9bbd616
Parth <parthinfo2002@gmail.com>
2025-06-19 00:04:19 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -6,4 +6,12 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+pool.connect()
+  .then(client => {
+    console.log('Connected to database successfully!');
+    client.release();
+  })
+  .catch(err => console.error('Connection error', err.stack));
+
+
 module.exports = pool;
