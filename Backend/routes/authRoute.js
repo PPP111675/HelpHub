b02d3f2366a5ac077e7704a079576f80a3e1d588
SapnaGagg <sapnagagg98555@gmail.com>
2025-06-19 00:03:36 +0530

    Update authRoute.js

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -28,6 +28,7 @@ router.post('/register', async (req, res) => {
 
     res.status(201).json({ message: 'Registration successful!' });
   } catch (err) {
+     console.error(err);
     res.status(500).json({ message: 'Server error.' });
   }
 });
