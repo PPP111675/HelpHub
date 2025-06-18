20078d109c6cd6be30b20502084093dc10c33cf0
Jass042 <jashanbrar070601@gmail.com>
2025-06-18 23:55:31 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/authRoute');
+const chatbotRoutes = require('./routes/chatbotRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-
+app.use("/api", chatbotRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
