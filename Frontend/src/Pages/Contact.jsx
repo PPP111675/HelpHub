2bc63eda738b7a7c69bd8343fa49984866f046ec
Parth <parthinfo2002@gmail.com>
2025-07-10 10:17:28 +0530

    Update Contact.jsx

--- Frontend/src/Pages/Contact.jsx
+++ Frontend/src/Pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:5000/api/contact', {
+    const response = await fetch('https://helphub-5.onrender.com/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
