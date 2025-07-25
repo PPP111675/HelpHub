00ea2c19e28e798683141ea117b2df57b7d1ee0c
Parth <parthinfo2002@gmail.com>
2025-07-25 17:33:35 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5005/api' });
+const api = axios.create({ baseURL: 'https://helphub-10.onrender.com/api' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
